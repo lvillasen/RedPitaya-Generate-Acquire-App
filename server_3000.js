cc5a07228e6c6cc0f6095a52a2e9e89d83c8acc7
Luis Villaseñor <lvillasen@gmail.com>
2023-05-21 23:09:44 -0600

    Update server_3000.js

--- server_3000.js
+++ server_3000.js
@@ -2,8 +2,12 @@ const express = require('express');
 const execSync = require('child_process').execSync;
 const app = express();
 const cors = require('cors');
+//const axios = require('axios');
 app.use(cors({ origin: "*", }))
 //app.use(express.json());
+app.disable('etag');
+app.disable('x-powered-by');
+
 
 app.get('/leds/:id', function(req, res) {
     const output = execSync('monitor 0x40000030 ' + req.params.id, { encoding: 'utf-8' });
@@ -65,18 +69,6 @@ app.get('/start_DAC', (req, res) => {
     const output2 = execSync('monitor 0x40000030 6' , { encoding: 'utf-8' });
     const output3 = execSync('monitor 0x40000030 0' , { encoding: 'utf-8' });
     const output = execSync(`python3 -c '${text}'`);
-});
-
-app.get('/stop_DAC', (req, res) => {
-    const text = req.query.text;
-    console.log('7.STOP DAC received by server')
-    const output1 = execSync('monitor 0x40000030 0' , { encoding: 'utf-8' });
-    const output2 = execSync('monitor 0x40000030 7' , { encoding: 'utf-8' });
-    const output3 = execSync('monitor 0x40000030 0' , { encoding: 'utf-8' });
-    const output = execSync(`${text}`);
-    const output4 = execSync('killall python3 ' , { encoding: 'utf-8' });
-    //const output = execSync('monitor 0x40200000 0x80' , { encoding: 'utf-8' });
-
 });
 
 app.get('/save', (req, res) => {
